8627490f52552e67b56ea5e5da53c3f9cafb9a4c
Claudio Zizza <zizza@flagbit.de>
2020-11-23 14:01:52 +0100

    [MAIL1UND1-3] Add namespace vendor prefix

--- src/Model/ParcelDelivery.php
+++ src/Model/ParcelDelivery.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Model;
+namespace Flagbit\SchemaOrgMailBody\Model;
 
 use InvalidArgumentException;
 
--- src/Model/ParcelDeliveryInterface.php
+++ src/Model/ParcelDeliveryInterface.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Model;
+namespace Flagbit\SchemaOrgMailBody\Model;
 
 interface ParcelDeliveryInterface extends AbstractOrderInterface
 {
